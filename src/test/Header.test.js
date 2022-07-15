5784ea47e1e3b182e2874a8da5b349c43ccfde2f
Maysa <maybsalvalaio@gmail.com>
2022-07-15 16:52:48 -0300

    Commit para dar pull

---
+++ src/test/Header.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './helpers/renderWithRouter';
+
+describe('Testes do componente Header', () => {
+  it('', () => {
+
+  });
+});
