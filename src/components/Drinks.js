8e10a20d16618a0d093869b5abe1f8db45043561
GusCabraal <gustavocabral97@hotmail.com>
2022-07-15 19:16:05 -0300

    criação do useFetch e requiso 20 concluido

--- src/components/Drinks.js
+++ src/components/Drinks.js
@@ -1,43 +1,53 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext } from 'react';
 import context from '../context/context';
+import useFetch from '../hooks/useFetch';
 
-function Foods() {
-  const { dataDrinks, setDataDrinks } = useContext(context);
-  const MAX_RECIPES = 12;
-
-  useEffect(() => {
-    async function fetchApi() {
-      const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
-      const response = await fetch(url);
-      const result = await response.json();
-      setDataDrinks(result);
-    }
-    fetchApi();
-  }, [setDataDrinks]);
-
+const urlDrinks = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
+const urlDrinksCategory = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list';
+const MAX_CATEGORIES = 5;
+const MAX_RECIPES = 12;
+function Drinks() {
+  const {
+    dataDrinks,
+    dataDrinksCategory,
+    setDataDrinks,
+    setDataDrinksCategory,
+  } = useContext(context);
+  useFetch(urlDrinks, setDataDrinks, MAX_RECIPES, 'drinks');
+  useFetch(urlDrinksCategory, setDataDrinksCategory, MAX_CATEGORIES, 'drinks');
   return (
-    <div className="cards">
-      {
-        Object.keys(dataDrinks).length > 0 && dataDrinks.drinks.map((drink, index) => (
-          index < MAX_RECIPES
-          && (
-            <div
-              key={ drink.idDrink }
-              className="card"
-              data-testid={ `${index}-recipe-card` }
-            >
-              <img
-                src={ drink.strDrinkThumb }
-                alt={ drink.strDrink }
-                data-testid={ `${index}-card-img` }
-              />
-              <h1 data-testid={ `${index}-card-name` }>{drink.strDrink}</h1>
-            </div>
-          )
-        ))
-      }
-    </div>
-  );
+    <div>
+      {(dataDrinks.length && dataDrinksCategory.length) > 0 && (
+        <div>
+          <div className="categories">
+            {dataDrinksCategory
+              .map(({ strCategory }) => (
+                <button
+                  type="button"
+                  key={ strCategory }
+                  data-testid={ `${strCategory}-category-filter` }
+                >
+                  {strCategory}
+                </button>))}
+          </div>
+          <div className="cards">
+            {dataDrinks.map((drink, index) => (
+              <div
+                key={ drink.idDrink }
+                className="card"
+                data-testid={ `${index}-recipe-card` }
+              >
+                <img
+                  src={ drink.strDrinkThumb }
+                  alt={ drink.strDrink }
+                  data-testid={ `${index}-card-img` }
+                />
+                <h1 data-testid={ `${index}-card-name` }>{drink.strDrink}</h1>
+              </div>
+            ))}
+          </div>
+        </div>)}
+    </div>);
 }
 
-export default Foods;
+export default Drinks;
--- src/context/Provider.js
+++ src/context/Provider.js
@@ -1,11 +1,35 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Context from './context';
+// import useFetch from '../hooks/useFetch';
 
+// const urlFood = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
+// const urlFoodCategory = 'https://www.themealdb.com/api/json/v1/1/list.php?c=list';
+// const urlDrinks = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
+// const urlDrinksCategory = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list';
+// const MAX_CATEGORIES = 5;
+// const MAX_RECIPES = 12;
 function Provider({ children }) {
-  const [dataFoods, setDataFoods] = useState({});
-  const [dataDrinks, setDataDrinks] = useState({});
-  const context = { dataFoods, setDataFoods, dataDrinks, setDataDrinks };
+  const [dataFoods, setDataFoods] = useState([]);
+  const [dataFoodsCategory, setDataFoodsCategory] = useState([]);
+  const [dataDrinks, setDataDrinks] = useState([]);
+  const [dataDrinksCategory, setDataDrinksCategory] = useState([]);
+
+  // useFetch(urlFood, setDataFoods, MAX_RECIPES, 'meals');
+  // useFetch(urlFoodCategory, setDataFoodsCategory, MAX_CATEGORIES, 'meals');
+  // useFetch(urlDrinks, setDataDrinks, MAX_RECIPES, 'drinks');
+  // useFetch(urlDrinksCategory, setDataDrinksCategory, MAX_CATEGORIES, 'drinks');
+
+  const context = {
+    dataFoods,
+    setDataFoods,
+    dataFoodsCategory,
+    setDataFoodsCategory,
+    dataDrinks,
+    setDataDrinks,
+    dataDrinksCategory,
+    setDataDrinksCategory,
+  };
   return (
     <Context.Provider value={ context }>
       { children }
