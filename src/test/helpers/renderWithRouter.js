989bab94c7b6867dfb003b5dbb316db1f35b094e
Maysa <maybsalvalaio@gmail.com>
2022-07-15 15:57:01 -0300

    Arrumando lint dos testes

--- src/test/helpers/renderWithRouter.js
+++ src/test/helpers/renderWithRouter.js
@@ -3,12 +3,12 @@ import { render } from '@testing-library/react';
 import { Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
 
-export const renderWithRouter = (component) => {
+const renderWithRouter = (component) => {
   const history = createMemoryHistory();
 
   return {
     ...render(
-      <Router history={history}>
+      <Router history={ history }>
         {component}
       </Router>,
     ),
