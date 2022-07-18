e3c88c20250be9500dd926483733d115c23b6f39
GusCabraal <gustavocabral97@hotmail.com>
2022-07-18 15:17:49 -0300

    requisitos 21 e 22 concluidos

--- src/components/Drinks.js
+++ src/components/Drinks.js
@@ -1,8 +1,8 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import context from '../context/context';
 import useFetch from '../hooks/useFetch';
 
-const urlDrinks = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
+const initialUrlDrinks = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
 const urlDrinksCategory = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list';
 const MAX_CATEGORIES = 5;
 const MAX_RECIPES = 12;
@@ -13,19 +13,41 @@ function Drinks() {
     setDataDrinks,
     setDataDrinksCategory,
   } = useContext(context);
+  const [urlDrinks, setUrlDrinks] = useState('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=');
+  const [filterAtual, setFilterAtual] = useState('');
   useFetch(urlDrinks, setDataDrinks, MAX_RECIPES, 'drinks');
   useFetch(urlDrinksCategory, setDataDrinksCategory, MAX_CATEGORIES, 'drinks');
+
+  const handleClick = (category) => {
+    if (category === '' || filterAtual === category) {
+      setUrlDrinks(initialUrlDrinks);
+      console.log('bora resetar');
+    } else {
+      console.log('bora trocar a categoria');
+      setUrlDrinks(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${category}`);
+    }
+    setFilterAtual(category);
+  };
   return (
     <div>
       {(dataDrinks.length && dataDrinksCategory.length) > 0 && (
         <div>
           <div className="categories">
+            <button
+              type="button"
+              key="All"
+              data-testid="All-category-filter"
+              onClick={ () => handleClick('') }
+            >
+              All
+            </button>
             {dataDrinksCategory
               .map(({ strCategory }) => (
                 <button
                   type="button"
                   key={ strCategory }
                   data-testid={ `${strCategory}-category-filter` }
+                  onClick={ () => handleClick(strCategory) }
                 >
                   {strCategory}
                 </button>))}
--- src/components/Foods.js
+++ src/components/Foods.js
@@ -1,8 +1,8 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import context from '../context/context';
 import useFetch from '../hooks/useFetch';
 
-const urlFood = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
+const initialUrlFood = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
 const urlFoodCategory = 'https://www.themealdb.com/api/json/v1/1/list.php?c=list';
 const MAX_CATEGORIES = 5;
 const MAX_RECIPES = 12;
@@ -13,22 +13,45 @@ function Foods() {
     setDataFoods,
     setDataFoodsCategory,
   } = useContext(context);
+  const [urlFood, setUrlFood] = useState('https://www.themealdb.com/api/json/v1/1/search.php?s=');
+  const [filterAtual, setFilterAtual] = useState('');
   useFetch(urlFood, setDataFoods, MAX_RECIPES, 'meals');
   useFetch(urlFoodCategory, setDataFoodsCategory, MAX_CATEGORIES, 'meals');
+
+  const handleClick = (category) => {
+    if (category === '' || filterAtual === category) {
+      setUrlFood(initialUrlFood);
+      console.log('bora resetar');
+    } else {
+      console.log('bora trocar a categoria');
+      setUrlFood(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`);
+    }
+    setFilterAtual(category);
+  };
   return (
     <div>
       {(dataFoods.length && dataFoodsCategory.length) > 0 && (
         <div>
           <div className="categories">
+            <button
+              type="button"
+              key="All"
+              data-testid="All-category-filter"
+              onClick={ () => handleClick('') }
+            >
+              All
+            </button>
             {dataFoodsCategory
               .map(({ strCategory }) => (
                 <button
                   type="button"
                   key={ strCategory }
                   data-testid={ `${strCategory}-category-filter` }
+                  onClick={ () => handleClick(strCategory) }
                 >
                   {strCategory}
                 </button>))}
+
           </div>
           <div className="cards">
             {dataFoods.map((food, index) => (
